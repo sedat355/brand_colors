a2a3e017c3640d338e587e90b9cd168b6c14ede0
Sedat Polat <sedatpolat355@gmail.com>
2024-08-03 15:09:20 +0300

    sidebar daki popup tamamlandı

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react"
+import Modal from "react-modal"
+import { GrClose } from "react-icons/gr"
+
 function Sidebar() {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+
+  const toggleModal = () => setModalIsOpen(!modalIsOpen)
+
   return (
     <>
       <aside className="sidebar">
@@ -15,11 +23,30 @@ function Sidebar() {
         <nav className="menu">
           <ul>
             <li>
-              <a>About BraundColors</a>
+              <a>Suggest a Brand</a>
+            </li>
+            <li>
+              <a onClick={toggleModal}>About BraundColors</a>
             </li>
           </ul>
         </nav>
       </aside>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={toggleModal}
+        className="about-modal"
+        overlayClassName="about-modal-overlay"
+      >
+        <button className="modal-close-btn" onClick={toggleModal}>
+          <GrClose/>
+        </button>
+        <h3>About BrandColors</h3>
+        <p>BrandColors was created by <strong>DesignBombs</strong>. The goal was to create a helpful reference for the brand color codes that are needed most often.
+        </p>
+        <p>It has been featured by Smashing Magazine, CSS-Tricks, Web Design Depot, Tuts+, and over <strong>2 million pageviews</strong>. There are now over 600 brands with 1600 colors and the collection is always growing.
+        </p>
+      </Modal>
     </>
   )
 }
