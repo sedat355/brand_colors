de66b7a7cc219437f70337e04db937cc20348f26
Sedat Polat <sedatpolat355@gmail.com>
2024-08-04 15:14:45 +0300

    search icon eklendi

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,11 @@
+import { GrSearch } from "react-icons/gr"
+
 function Search(props) {
   return(
     <div className="search">
+      <div className="icon">
+      <GrSearch/>
+      </div>
       <input placeholder="Search brands.." />
     </div>
   )
