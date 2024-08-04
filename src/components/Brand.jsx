76c5ffb3d3be9ac788be0e147da38ff6ce43c492
Sedat Polat <sedatpolat355@gmail.com>
2024-08-04 16:38:01 +0300

    Brand içinde colors' lar listelendi

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -2,6 +2,10 @@ function Brand({brand}) {
   return(
     <div className="brand">
       <h5>{brand.title}</h5>
+
+      <div className="brand-colors">
+        {brand.colors.map( (color, i) => <span key={i}>{color}</span>)}
+      </div>
     </div>
   )
 }
