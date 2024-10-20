8bcc13445c774520ce3f9cb4af63b19214a7a8b9
Sedat Polat <sedatpolat355@gmail.com>
2024-10-20 14:28:01 +0300

    download işlemi tamamlandı

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,15 +1,75 @@
 import {useContext} from "react"
 import MainContext from "./MainContext"
 import { GrLink,GrDownload,GrClose } from "react-icons/gr"
+import {useState} from "react"
+import {useEffect} from "react"
 
 const Download = () => {
-  const { selectedBrands,setSelectedBrands } = useContext(MainContext)
+  const { selectedBrands,setSelectedBrands,brands } = useContext(MainContext)
+  const [downloadUrl,setDownloadUrl] = useState()
+  const [cssMethod, setCssMethod] = useState('css')
+
+  const getLink = () => {
+    prompt('Here\'s the URL to share',`http://localhost:5173/collection/${selectedBrands.join(',')}`)
+  }
+
+  useEffect(() => {
+    if(selectedBrands.length > 0) {
+      let output = ''
+
+      switch (cssMethod) {
+        case 'css':
+          output += ':root {\n'
+          selectedBrands.map(slug => {
+            let brand = brands.find(brand => brand.slug === slug)
+            brand.colors.map((color,key) => {
+              output += `--${slug}-${key}: #${color};\n`
+            })
+          })
+          output += '}'
+          break;
+        case 'scss':
+          selectedBrands.map(slug => {
+            let brand = brands.find(brand => brand.slug === slug)
+            brand.colors.map((color,key) => {
+              output += `\$${slug}-${key}: #${color};\n`
+            })
+          })
+          break;
+        case 'less':
+          selectedBrands.map(slug => {
+            let brand = brands.find(brand => brand.slug === slug)
+            brand.colors.map((color,key) => {
+              output += `@${slug}-${key}: #${color};\n`
+            })
+          })
+          break;
+      }
+
+      const blob = new Blob([output])
+      const url = URL.createObjectURL(blob)
+      setDownloadUrl(url)
+
+      return () => {
+        URL.revokeObjectURL(url)
+        setDownloadUrl('')
+      }
+    }
+  },[selectedBrands,cssMethod])
 
   return(
     <div className="download">
       <div className="actions">
-        <a><GrDownload/></a>
-        <button><GrLink/></button>
+        <select className="download-list" onChange={(e) => setCssMethod(e.target.value)}>
+          <option value='css'>CSS</option>
+          <option value='scss'>SCSS</option>
+          <option value='less'>LESS</option>
+        </select>
+        <a download={`brands.${cssMethod}`} href={downloadUrl}><GrDownload/></a>
+        
+        <button
+          onClick={getLink}
+        ><GrLink/></button>
 
       </div>
       <div className="selected" onClick={	() => setSelectedBrands([])}>
